61996df840bc3d618c7020378e8d82e1edf17b01
Salome <sabdomaria7@gmail.com>
2022-12-02 09:40:46 -0300

    optimizando

---
+++ Js/productos.js
@@ -0,0 +1,21 @@
+let ceramico = [ {nombre:"Ceramico simil madera", precio: "$980", medida: "2,02m", stock: "20", imgUrl: "../Imagenes/Ceramico 1.jpg"},
+{nombre:"Ceramico blanco y negro", precio: "$980", medida: "2,61m", stock: "10", imgUrl: "../Imagenes/Ceramico 2.jpg"},
+{nombre:"Ceramico en mandala", precio: "$980", medida: "2,02m", stock: "15", imgUrl: "../Imagenes/Ceramico 3.jpg"},
+]
+
+let alacenaybajo = [
+    {nombre: "Melamina negro", precio:"$5000", medida:"1,20m ", stock:"10", imgUrl:"../Imagenes/Alacenas y bajom.jpg"},
+    {nombre: "Melamina blanco", precio:"$5500", medida:"1,20m", stock:"15", imgUrl:"../Imagenes/Alacenas y bajome.jpg"}
+]
+
+let griferia = [
+    {nombre:"Juego romano", precio:"$4500", stock:"5", imgUrl:"../Imagenes/griferia1.jpg"},
+    {nombre:"Juego italiano", precio:"$6000", stock:"3", imgUrl:"../Imagenes/griferia2.jpg"},
+    {nombre:"Juego FV", precio:"$5000", stock:"4", imgUrl:"../Imagenes/griferia5.webp"},
+    {nombre:"Juego Escoces", precio:"$3600", stock:"12", imgUrl:"../Imagenes/griferia3.jpg"},
+    {nombre:"Juego economico", precio:"$2000", stock:"35", imgUrl:"../Imagenes/griferia4.jpg"},
+]
+
+let `ceramico` = document.getElementsByClassName("producto")
+
+console.dir(`ceramico`)
